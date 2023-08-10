7cb24d0b45882c39bf1822ac780cde2f53ddfb15
Samcaleb <samcalebjames2017@gmail.com>
2023-08-10 12:37:13 +0530

    db localhost

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default{
     port:5001,
     host:'localhost',
-    dbUri:"mongodb://127.0.0.1:27017/mydb",
-    // dbUri:"mongodb://0.0.0.1:27017/mydb",
+    // dbUri:"mongodb://127.0.0.1:27017/mydb",
+    dbUri:"mongodb://localhost:27017/mydb",
 }
